639b8f030e281e7515f0c6e40f9e2db6b717401e
alagunoff <lagunov.artamon@gmail.com>
2025-04-03 11:22:04 +0300

    chore: rename title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Front-end app template</h1>
+      <h1 className="text-4xl font-bold">Web app template</h1>
     </main>
   );
 }
