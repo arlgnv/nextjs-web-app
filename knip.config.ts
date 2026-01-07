294b75f59d51f43cc89d8adf5ecd0eee426cc5a7
arlgnv <arlgnv@outlook.com>
2026-01-07 13:40:44 +0300

    build: update dependencies

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,9 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   ignoreDependencies: ['postcss'],
+  commitlint: {
+    config: 'commitlint.config.mts',
+  },
   'lint-staged': {
     config: 'lint-staged.config.mts',
   },
